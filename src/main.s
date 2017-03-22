691286930dcbc1e1c8f947ebeb72fdb57795b8b3
Philippe Gauthier <philippe.gauthier@deuxpi.ca>
2017-03-21 22:14:23 -0400

    Music!

--- src/main.s
+++ src/main.s
@@ -1,22 +1,62 @@
+.segment "ZEROPAGE": zeropage
+
+_thi: .res 1, $00
+_tlo: .res 1, $00
+_tmp1: .res 1, $00
+_tmp2: .res 1, $00
+
 .segment "CODE"
 main:
     lda $900e ; 4
     ora #$0f  ; 2
     sta $900e ; 4
 
-loop:
-    lda $900c ; 4
-    adc #$01  ; 2
-    ora #$80  ; 2
+    lda #$d0
     sta $900c ; 4
 
-    ldy #$3f  ; 2
-wait1:
-    ldx #$ff  ; 2
-wait2:
-    dex       ; 2
-    bne wait2 ; 2/3
-    dey       ; 2
-    bne wait1 ; 2/3
+    lda #0
+    ldx #0
+    ldy #0
+
+loop:
+    ora #$80
+    sta $900c
+
+    and #$0f  ; 2
+    sta $900e ; 4
+
+    ldx z:_tlo
+    ldy z:_thi
+    inx
+    bne _nocarry
+    iny
+_nocarry:
+    stx z:_tlo
+    sty z:_thi
+
+    clc
+    lda z:_tlo
+    adc z:_tlo
+    adc z:_tlo
+    sta z:_tmp1
+    adc z:_tlo
+    adc z:_tlo
+    sta z:_tmp2
+
+    lda z:_thi
+    asl
+    and z:_tmp2
+    sta z:_tmp2
+
+    lda z:_thi
+    lsr
+    lsr
+    and z:_tmp1
+    ora z:_tmp2
+
+    ldy #$ff
+wait:
+    dey
+    bne wait
 
     jmp loop  ; 3
